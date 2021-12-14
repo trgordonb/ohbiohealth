9cc7c8fa62d383a68cbfb29a26ff60086ddc1e13
Gordon Cheng <gordoncheng@outlook.com>
2021-12-14 22:57:48 +0800

    auth startup message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,6 +6,7 @@ import { natsWrapper } from './nats-wrapper';
 import { app } from './app';
 
 const start = async () => {  
+  console.log("Starting...")
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
