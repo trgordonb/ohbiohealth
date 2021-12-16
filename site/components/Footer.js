cf2c7c9bcb04971bb40e4ab4f21a0bfdaad5349f
Gordon Cheng <gordoncheng@outlook.com>
2021-12-16 18:17:41 +0800

    update footer logo

--- site/components/Footer.js
+++ site/components/Footer.js
@@ -1,10 +1,20 @@
 import Link from 'next/link'
 import { FaFacebook, FaInstagram, FaYoutube } from 'react-icons/fa'
+import Image from 'next/image'
 import { useTranslation } from 'react-i18next'
+import { useState, useEffect } from 'react'
 import styles from '../styles/Footer.module.css'
+import GooglePlayIconEN from '../public/images/googleplaybadgeEN.png'
+import GooglePlayIconZH from '../public/images/googleplaybadgeZH.png'
 
 export default function Footer() {
     const { t, i18n } = useTranslation()
+    const [GooglePlayIcon, setGooglePlayIcon] = useState(i18n.language === 'en' ? GooglePlayIconEN : GooglePlayIconZH)
+
+    useEffect(() => {
+        setGooglePlayIcon(i18n.language === 'en' ? GooglePlayIconEN : GooglePlayIconZH)
+    },[i18n.language])
+
     return (   
         <footer className={styles.footer}>
             <div>
@@ -12,6 +22,7 @@ export default function Footer() {
                     <Link href="https://www.youtube.com/channel/UCXjd7VSLVHL3R3GUZ_LqtQw"><FaYoutube size={25} className={styles.icon}/></Link>
                     <Link href="https://www.instagram.com/ohbiohealth"><FaInstagram size={25} className={styles.icon}/></Link>
                     <Link href="https://www.facebook.com/OnourHolistic"><FaFacebook size={25} className={styles.icon}/></Link>
+                    <Link href="https://play.google.com/store/apps/details?id=org.ohbiohealth.healthtracker"><Image src={GooglePlayIcon} width={100} height={40}/></Link>
                 </p>
                 <p>Copyright &copy; OH Biohealth 2021</p>
             </div>
