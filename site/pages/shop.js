6ee49faecdeeeab72e60c6169b58e02c2f9e03ec
Gordon Cheng <gordoncheng@outlook.com>
2021-12-23 22:58:31 +0800

    use getServerSideProps for shop page

--- site/pages/shop.js
+++ site/pages/shop.js
@@ -3,7 +3,7 @@ import { useTranslation } from 'react-i18next'
 import { ProductBrowser, BuyNowButton } from '@ecwid/nextjs-ecwid-plugin'
 
 
-ShopPage.getInitialProps = () => {
+export function getServerSideProps() {
     const storeId = process.env.NEXT_PUBLIC_ECWID_STOREID
     console.log('Server value:', storeId)
     return { 
