82c028226837e8491ea47f8557bafbc1bcf43eb5
trgordonb <gordoncheng@outlook.com>
2022-03-16 17:52:11 +0800

    revamp signin signup pages

--- site/pages/account/signup.js
+++ site/pages/account/signup.js
@@ -42,102 +42,101 @@ export default function SignUpPage() {
   },[errors])
 
   return (
-    <div className='py-16'>
-      <div className="w-full md:w-96 md:max-w-full mx-auto">
-        <h1 className='mt-10 mb-10 px-4 font-bold text-3xl'>{t('register')}</h1>
-      </div>
+    <section className="h-full gradient-form bg-gray-200 md:h-screen">
       <ToastContainer />
-        <div className="w-full md:w-96 md:max-w-full mx-auto p-4 bg-indigo-100">
-          <div className="p-6 border border-gray-300 sm:rounded-md">
-          <form onSubmit={handleSubmit}>
-            <div>
-              <label className="block mb-6">
-                <span className="text-gray-700">{t('email')}</span>
-                <input
-                  type='email'
-                  id='email'
-                  name='email'
-                  className="
-                    block
-                    w-full
-                    mt-1
-                    border-gray-300
-                    rounded-md
-                    shadow-sm
-                    focus:border-indigo-300
-                    focus:ring
-                    focus:ring-indigo-200
-                    focus:ring-opacity-50
-                  "
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </label>
-              <label className="block mb-6">
-                <span className="text-gray-700">{t('password')}</span>
-                <input
-                  type='password'
-                  id='password'
-                  name='password'
-                  className="
-                    block
-                    w-full
-                    mt-1
-                    border-gray-300
-                    rounded-md
-                    shadow-sm
-                    focus:border-indigo-300
-                    focus:ring
-                    focus:ring-indigo-200
-                    focus:ring-opacity-50
-                  "
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </label>
-              <label className="block mb-6">
-                <span className="text-gray-700">{t('confirmpwd')}</span>
-                <input
-                  type='password'
-                  id='passwordConfirm'
-                  name='passwordConfirm'
-                  className="
-                    block
-                    w-full
-                    mt-1
-                    border-gray-300
-                    rounded-md
-                    shadow-sm
-                    focus:border-indigo-300
-                    focus:ring
-                    focus:ring-indigo-200
-                    focus:ring-opacity-50
-                  "
-                  value={passwordConfirm}
-                  onChange={(e) => setPasswordConfirm(e.target.value)}
-                />
-              </label>
-              <input 
-                type='submit'   
-                value={t('register')} 
-                className="
-                  h-10
-                  px-5
-                  text-indigo-100
-                  bg-indigo-700
-                  rounded-lg
-                  transition-colors
-                  duration-150
-                  focus:shadow-outline
-                  hover:bg-indigo-800
-                "
-              />
+          <div className="container mx-auto py-12 px-6 h-full">
+            <div className="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
+              <div className="xl:w-10/12">
+                <div className="block bg-white shadow-lg rounded-lg">
+                  <div className="lg:flex lg:flex-wrap g-0">
+                    <div className="lg:w-6/12 px-4 md:px-0">
+                      <div className="md:p-12 md:mx-6">
+                        <div className="text-center">
+                          <img
+                            className="mx-auto w-48 mb-20"
+                            src="/OHLogo.jpg"
+                            alt="logo"
+                          />
+                        </div>
+                        <form>
+                          <p className="mb-4">{t('register')}</p>
+                          <div className="mb-4">
+                            <input
+                              type="text"
+                              className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                              id="email"
+                              name="email"
+                              placeholder={t('email')}
+                              value={email}
+                              onChange={(e) => setPassword(e.target.value)}
+                            />
+                          </div>
+                          <div className="mb-4">
+                            <input
+                              type="password"
+                              className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                              id="password"
+                              name="password"
+                              placeholder={t('password')}
+                              value={password}
+                              onChange={(e) => setPassword(e.target.value)}
+                            />
+                          </div>
+                          <div className="mb-4">
+                            <input
+                              type="password"
+                              className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                              id="confirmpassword"
+                              name="confirmpassword"
+                              placeholder={t('confirmpwd')}
+                              value={passwordConfirm}
+                              onChange={(e) => setPassword(e.target.value)}
+                            />
+                          </div>
+                          <div className="text-center pt-1 mb-12 pb-1">
+                            <button
+                              className="inline-block px-6 py-2.5 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full mb-3 bg-gradient-to-r from-indigo-500 to-cyan-500"
+                              type="button"
+                              data-mdb-ripple="true"
+                              data-mdb-ripple-color="light"
+                              onClick={()=> handleSubmit()}
+                            >
+                              {t('submit')}
+                            </button>
+                          </div>
+                          <div className="flex items-center justify-between pb-6">
+                            <p className="mb-0 mr-2">{t('haveaccount')}</p>
+                            <button
+                              type="button"
+                              class="inline-block px-6 py-2 border-2 border-cyan-600 text-cyan-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
+                              data-mdb-ripple="true"
+                              data-mdb-ripple-color="light"
+                            >
+                              <Link href='/account/signin'>{t('login')}</Link>
+                            </button>
+                            
+                          </div>
+                        </form>
+                      </div>
+                    </div>
+                    <div
+                      className="lg:w-6/12 flex items-center lg:rounded-r-lg rounded-b-lg lg:rounded-bl-none bg-gradient-to-r from-indigo-500 to-cyan-500"
+                    >
+                      <div className="text-white px-4 py-6 md:p-12 md:mx-6">
+                        <h4 className="text-xl font-semibold mb-6">We are more than just a company</h4>
+                        <p className="text-sm">
+                          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                          consequat.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-        </form>
-      </div>
-      <p className='mt-2 p-6'>
-        {t('haveaccount')} <Link href='/account/signin'>{t('signin')}</Link>
-      </p>
-    </div>
-  </div>)
+        </section>
+  )
 }
