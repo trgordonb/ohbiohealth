abb6ab3f1de5adc82bc5d6dc8db8fbea66d3ce27
trgordonb <gordoncheng@outlook.com>
2022-03-16 17:57:48 +0800

    fix css className type

--- site/pages/account/signin.js
+++ site/pages/account/signin.js
@@ -95,7 +95,7 @@ export default function SigninPage() {
                             <p className="mb-0 mr-2">{t('noaccount')}</p>
                             <button
                               type="button"
-                              class="inline-block px-6 py-2 border-2 border-cyan-600 text-cyan-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
+                              className="inline-block px-6 py-2 border-2 border-cyan-600 text-cyan-600 font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
                               data-mdb-ripple="true"
                               data-mdb-ripple-color="light"
                             >
