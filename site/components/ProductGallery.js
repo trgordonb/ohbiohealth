3746fcc537e3f5c84984d4500c505aa6ffafb702
trgordonb <gordoncheng@outlook.com>
2022-03-18 10:47:35 +0800

    add overflow scroll to product gallery

--- site/components/ProductGallery.js
+++ site/components/ProductGallery.js
@@ -12,7 +12,7 @@ export default function ProductGallery(content) {
           <a href="#" className="relative inline-block duration-300 ease-in-out transition-transform transform hover:-translate-y-2 w-full">
             <div className="shadow p-4 rounded-lg bg-white">
               <div className="flex justify-center relative rounded-lg overflow-hidden h-[500px]">
-                <div className="transition-transform duration-500 transform ease-in-out hover:scale-105 w-full bg-gradient-to-r from-indigo-500 to-cyan-500 p-5">
+                <div className="transition-transform duration-500 transform ease-in-out hover:scale-105 w-full bg-gradient-to-r from-indigo-500 to-cyan-500 p-5 overflow-auto">
                   <h2 className="text-3xl text-gray-800 font-semibold px-5">BM</h2>
                     <img className='mx-auto object-center p-4 float-right bg-white ml-10' src={`${router.basePath}/images/BM.png`} alt='BM alt text' width={200} height={200}/>   
                     <ReactMarkdown className="mt-6 text-base text-gray-800 leading-6 whitespace-pre-wrap list-disc px-5">
@@ -28,7 +28,7 @@ export default function ProductGallery(content) {
           <a href="#" className="relative inline-block duration-300 ease-in-out transition-transform transform hover:-translate-y-2 w-full">
             <div className="shadow p-4 rounded-lg bg-white">
               <div className="flex justify-center relative rounded-lg overflow-hidden h-[500px]">
-                <div className="transition-transform duration-500 transform ease-in-out hover:scale-105 w-full bg-gradient-to-r from-indigo-500 to-cyan-500 p-5">
+                <div className="transition-transform duration-500 transform ease-in-out hover:scale-105 w-full bg-gradient-to-r from-indigo-500 to-cyan-500 p-5 overflow-auto">
                   <h2 className="text-3xl text-gray-800 font-semibold px-5">QM</h2>
                     <img className='mx-auto object-center p-4 float-right bg-white ml-10' src={`${router.basePath}/images/QE.png`} alt='QM alt text' width={200} height={200}/>   
                     <ReactMarkdown className="mt-6 text-base text-gray-800 leading-6 whitespace-pre-wrap list-disc px-5">
@@ -44,10 +44,10 @@ export default function ProductGallery(content) {
           <a href="#" className="relative inline-block duration-300 ease-in-out transition-transform transform hover:-translate-y-2 w-full">
             <div className="shadow p-4 rounded-lg bg-white">
               <div className="flex justify-center relative rounded-lg overflow-hidden h-[500px]">
-                <div className="transition-transform duration-500 transform ease-in-out hover:scale-105 w-full bg-gradient-to-r from-indigo-500 to-cyan-500 p-5">
+                <div className="transition-transform duration-500 transform ease-in-out hover:scale-105 w-full bg-gradient-to-r from-indigo-500 to-cyan-500 p-5 overflow-auto">
                   <h2 className="text-3xl text-gray-800 font-semibold px-5">BES</h2>
                     <img className='mx-auto object-center p-4 float-right bg-white ml-10' src={`${router.basePath}/images/BES.png`} alt='BES alt text' width={200} height={200}/>   
-                    <ReactMarkdown className="mt-6 text-base text-gray-800 leading-6 whitespace-pre-wrap list-disc px-5">
+                    <ReactMarkdown className="mt-6 text-base text-gray-800 leading-6 whitespace-pre-wrap list-disc px-5 overflow-scroll">
                       {content.content.BESContent}
                     </ReactMarkdown>
                 </div>
@@ -60,7 +60,7 @@ export default function ProductGallery(content) {
           <a href="#" className="relative inline-block duration-300 ease-in-out transition-transform transform hover:-translate-y-2 w-full">
             <div className="shadow p-4 rounded-lg bg-white">
               <div className="flex justify-center relative rounded-lg overflow-hidden h-[500px]">
-                <div className="transition-transform duration-500 transform ease-in-out hover:scale-105 w-full bg-gradient-to-r from-indigo-500 to-cyan-500 p-5">
+                <div className="transition-transform duration-500 transform ease-in-out hover:scale-105 w-full bg-gradient-to-r from-indigo-500 to-cyan-500 p-5 overflow-auto">
                   <h2 className="text-3xl text-gray-800 font-semibold px-5">SEG</h2>
                     <img className='mx-auto object-center p-4 float-right bg-white ml-10' src={`${router.basePath}/images/SEG.png`} alt='BM alt text' width={200} height={200}/>   
                     <ReactMarkdown className="mt-6 text-base text-gray-800 leading-6 whitespace-pre-wrap list-disc px-5">
