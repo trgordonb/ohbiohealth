ea26c71b20e449b213048786e79f07cb47a53f18
trgordonb <gordoncheng@outlook.com>
2022-04-15 22:49:46 +0800

    change to AWS EKS CI/CD pipeline

--- site-oh/api/build-client.js
+++ site-oh/api/build-client.js
@@ -6,7 +6,7 @@ export default ({ req }) => {
 
     return axios.create({
       //baseURL:'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
-      baseURL : 'https://ohbiohealth.xyz',
+      baseURL : 'https://linkaiq.xyz',
       headers: req.headers,
     });
   } else {
