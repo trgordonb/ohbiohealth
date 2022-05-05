69645ac26277afe821087ef6de97d1586472b896
trgordonb <gordoncheng@outlook.com>
2022-05-06 01:28:10 +0800

    fix nextjs version

--- site-oh/pages/shop.js
+++ site-oh/pages/shop.js
@@ -5,7 +5,6 @@ import Product from '../components/Product'
 
 const ShopPage = ({ products }) => {
     const { t, i18n } = useTranslation()
-	console.log(products)
     return (        
 		<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 w-full">
 			{
